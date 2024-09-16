b06885cd4d8198f066e22c2aae199e45cb2f8b25
RenzoA <u202113612@upc.edu.pe>
2024-09-16 13:11:22 -0500

    feat: complements

--- src/app/public/components/navigation-bar/navigation-bar.component.ts
+++ src/app/public/components/navigation-bar/navigation-bar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {FooterContentComponent} from "../footer-content/footer-content.component";
+import {HomeComponent} from "../../../nutri-send/components/home/home.component";
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
   imports: [
-    FooterContentComponent
+    FooterContentComponent,
+    HomeComponent
   ],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css'
